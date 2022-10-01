0679487e9b48d2dde086150ddaacdce730c65f01
stasbezhan <krig.ua1968@gmail.com>
2022-10-01 19:39:36 +0300

    Package added

---
+++ YYApi_Voyo/Model/VideoModel.swift
@@ -0,0 +1,12 @@
+//
+//  VideoModel.swift
+//  YYApi_Voyo
+//
+//  Created by Stas Bezhan on 01.10.2022.
+//
+
+import Foundation
+
+struct Video {
+    
+}
--- YYApi_Voyo/Supporting Files/Constants.swift
+++ YYApi_Voyo/Supporting Files/Constants.swift
@@ -10,4 +10,5 @@ import Foundation
 struct Constants {
     public static let topPlayilistCell = "TopPlaylistCell"
     public static let botPlaylistCell = "BotPlaylistCell"
+    public static let apiKey = "AIzaSyA2R0Fdp-7dZwnchEr1w96XO7bVeQzQ_kA"
 }
